fbc1d32775948e7e9e942701a9d240a8c2bd203f
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-05-30 00:36:27 -0500

    exersize solved

--- reindeer.js
+++ reindeer.js
@@ -3,3 +3,18 @@ var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donne
 
 
 var hohohoElement = document.getElementById("reindeer");
+
+var loopCount = reindeer.length;
+
+var currentReindeer = "";
+
+var currentColor = "";
+
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+	currentColor = colors[loopTracker];
+	currentReindeer = reindeer[loopTracker];
+	var reindeerParagraphTag = document.createElement('p');
+	var reindeerText = document.createTextNode(currentColor + ' ' + currentReindeer);
+	reindeerParagraphTag.appendChild(reindeerText);
+	hohohoElement.appendChild(reindeerParagraphTag);
+}
